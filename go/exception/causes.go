8e7942923731b62648611e0067fbdf3ea31026cc
Jahns Christian Albert <vanzrael@gmail.com>
2024-11-06 09:25:30 +0700

    add exception cause mapping to http status

--- go/exception/causes.go
+++ go/exception/causes.go
@@ -1,6 +1,54 @@
 package exception
 
-var CAUSE_INTERNAL = "internal"
+import "net/http"
+
 var CAUSE_USER = "user"
 var CAUSE_UNAUTHORIZED = "unauthorized"
 var CAUSE_FORBIDDEN = "forbidden"
+var CAUSE_NOT_FOUND = "not found"
+var CAUSE_INTERNAL = "internal"
+
+type causeStatusMapping struct {
+	cause  string
+	status int
+}
+
+func NewCauseStatusMapping(cause string, status int) causeStatusMapping {
+	return causeStatusMapping{
+		cause:  cause,
+		status: status,
+	}
+}
+
+func (m causeStatusMapping) Cause() string {
+	return m.cause
+}
+
+func (m causeStatusMapping) Status() int {
+	return m.status
+}
+
+func GetHttpStatus(cause string) int {
+	status := http.StatusTeapot
+	cause_mappings := getCauseStatusMapping()
+	for _, mapping := range cause_mappings {
+		if cause == mapping.Cause() {
+			status = mapping.Status()
+			break
+		}
+	}
+
+	return status
+}
+
+func getCauseStatusMapping() []causeStatusMapping {
+	cause_mappings := []causeStatusMapping{
+		NewCauseStatusMapping(CAUSE_USER, http.StatusBadRequest),
+		NewCauseStatusMapping(CAUSE_UNAUTHORIZED, http.StatusUnauthorized),
+		NewCauseStatusMapping(CAUSE_FORBIDDEN, http.StatusNotFound),
+		NewCauseStatusMapping(CAUSE_NOT_FOUND, http.StatusNotFound),
+		NewCauseStatusMapping(CAUSE_INTERNAL, http.StatusInternalServerError),
+	}
+
+	return cause_mappings
+}
