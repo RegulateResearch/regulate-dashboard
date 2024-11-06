d0dc4f8d42f555e1c3bce83ac35aa1a94085a38d
Jahns Christian Albert <vanzrael@gmail.com>
2024-11-06 16:27:46 +0700

    feat: add auth repository impl

---
+++ go/repository/auth.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"frascati/entity"
+	"frascati/exception"
+	repository_exception "frascati/repository/exception"
+)
+
+type AuthRepository interface {
+	Add(ctx context.Context, newUserData entity.UserWrite) (entity.User, exception.Exception)
+	FindByUsername(ctx context.Context, username string) (entity.User, exception.Exception)
+}
+
+type authRepositoryImpl struct {
+	db *sql.DB
+}
+
+func (r authRepositoryImpl) FindByUsername(ctx context.Context, username string) (entity.User, exception.Exception) {
+	query := `
+		SELECT id, username, password, user_role
+		FROM users
+		WHERE username = $1
+		LIMIT 1
+	`
+
+	var user entity.User
+	err := r.db.QueryRowContext(ctx, query, username).Scan(&user.ID, &user.Username, &user.Password, &user.Role)
+	if err != nil {
+		var empty entity.User
+		if errors.Is(err, sql.ErrNoRows) {
+			return empty, repository_exception.CreateRecordNotFoundException(err, "auth", "record not found")
+		}
+
+		return empty, repository_exception.CreateDBException(err, "auth", "something is wrong in our end")
+	}
+
+	return user, nil
+}
+
+func (r authRepositoryImpl) IsExist(ctx context.Context, username string) (bool, error) {
+	query := `SELECT 1 FROM users WHERE username = $1`
+	res, err := r.db.ExecContext(ctx, query, username)
+	if err != nil {
+		return false, repository_exception.CreateDBException(err, "auth", "something is wrong in our end")
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return false, repository_exception.CreateDBException(err, "auth", "something is wrong in our end")
+	}
+
+	return rowsAffected > 0, nil
+}
---
+++ go/repository/exception/exception.go
@@ -0,0 +1,14 @@
+package exception
+
+import (
+	"fmt"
+	"frascati/exception"
+)
+
+func CreateDBException(err error, domain string, message string) exception.Exception {
+	return exception.NewBaseException(exception.CAUSE_INTERNAL, fmt.Sprintf("%s/repository", domain), message, err)
+}
+
+func CreateRecordNotFoundException(err error, domain string, message string) exception.Exception {
+	return exception.NewBaseException(exception.CAUSE_NOT_FOUND, fmt.Sprintf("%s/repository", domain), message, err)
+}
