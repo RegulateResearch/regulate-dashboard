a813b9ccfd8ee60f9df10dc74ea963e40c8bbd95
Jahns Christian Albert <vanzrael@gmail.com>
2026-02-27 17:33:58 +0700

    finally deployable at local, will test at coolify

--- sveltekit/src/lib/server/db/index.ts
+++ sveltekit/src/lib/server/db/index.ts
@@ -3,6 +3,9 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
+
+const databaseUrl = `postgres://${env.DB_USERNAME}:${env.DB_PASSWORD}@${env.DB_HOST}:5432/${env.DB_NAME}`
+
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = postgres(env.DATABASE_URL);
