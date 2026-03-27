d344092e8348deb3e1ba6c7bb5dbe6b632e3f55c
RegulateResearch <michael.jf.jm@gmail.com>
2026-03-27 18:53:27 +0900

    temp: temporary route for authentication testing, should be deleted later

---
+++ sveltekit/src/routes/(authed)/after-login/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+
+	let data = $props();
+</script>
+
+<p>Congrats, you reached this page.</p>
+
+<form method="POST">
+  <Button type="submit">Logout</Button>
+</form>
