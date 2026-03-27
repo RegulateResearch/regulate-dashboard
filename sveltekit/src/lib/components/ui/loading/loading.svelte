3266579e66107502b3daeafa094d2eec69687f53
RegulateResearch <michael.jf.jm@gmail.com>
2026-03-27 18:50:32 +0900

    feat: add temporary loading screen component (should be revised later in dedicated task)

---
+++ sveltekit/src/lib/components/ui/loading/loading.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Logo } from '$lib/components/ui/logo/index';
+</script>
+
+<div
+	class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-yellow-50/25 backdrop-blur-md"
+>
+	<Logo animated loop></Logo>
+</div>
