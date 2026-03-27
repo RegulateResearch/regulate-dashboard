eade65feeec91319a682e834ee7dca408252597e
RegulateResearch <michael.jf.jm@gmail.com>
2026-03-27 18:54:43 +0900

    feat: login feature

---
+++ sveltekit/src/lib/server/api/auth/schema.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+export const sessionResponseSchema = z.object({
+  message: z.string(),
+  data: z.optional(z.object({
+    id: z.number(),
+    role: z.string()
+  })),
+});
+export type SessionResponse = z.infer<typeof sessionResponseSchema>;
+
+export const loginRequestSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+export type LoginRequest = z.infer<typeof loginRequestSchema>;
+
+export const loginResponseSchema = z.object({
+  message: z.string(),
+  data: z.string(),
+});
+export type LoginResponse = z.infer<typeof loginResponseSchema>;
+
+export const registerRequestSchema = z.object({
+  email: z.email(),
+  username: z.string(),
+  password: z.string()
+})
+export type RegisterRequest = z.infer<typeof registerRequestSchema>;
+
+export const registerResponseSchema = z.object({
+  id: z.number(),
+  email: z.email(),
+  username: z.string(),
+  role: z.number()
+})
+export type RegisterResponse = z.infer<typeof registerResponseSchema>;
---
+++ sveltekit/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import DoorOpen from '@lucide/svelte/icons/door-open';
+	import { Button, buttonVariants } from '$lib/components/ui/button';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import { SRLContent } from '$lib/components/interactiveContents/srlContents';
+
+	let { children } = $props();
+</script>
+
+<Tooltip.Root>
+	<Tooltip.Trigger class="fixed top-4 left-4">
+		<Button variant="ghost" class="h-12 w-12" href="/">
+			<DoorOpen class="size-6 text-neutral-600" />
+		</Button>
+	</Tooltip.Trigger>
+	<Tooltip.Content collisionPadding={16}>
+		<p>Kembali ke Halaman Utama</p>
+	</Tooltip.Content>
+</Tooltip.Root>
+
+<section
+	class="flex h-screen w-full flex-row items-center justify-center gap-16 lg:gap-32 lg:px-16"
+>
+	<div class="regulate-logo-content hidden lg:block">
+		<SRLContent class="w-0.8 max-h-0.8 h-100"></SRLContent>
+	</div>
+	{@render children()}
+</section>
