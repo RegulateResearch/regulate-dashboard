f392acc16fdeffa78b8e622b0b283fcf80be2602
RegulateResearch <michael.jf.jm@gmail.com>
2026-04-03 19:14:19 +0900

    temp: sso exploration for testing in dev

--- sveltekit/src/routes/(auth)/sso/+page.server.ts
+++ sveltekit/src/routes/(auth)/sso/+page.server.ts
@@ -1,18 +1,16 @@
 import { fail } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
+import { message } from "valibot"
 
 export const load: PageServerLoad = async ({ url, fetch }) => {
   const ticket = url.searchParams.get('ticket')
-  const service = encodeURIComponent("http://localhost:5173/sso")
+  const service = encodeURIComponent("http://localhost:5173/")
   const validationUrl = `https://sso.ui.ac.id/cas2/serviceValidate?ticket=${ticket}&service=${service}`
-  console.log(validationUrl)
   const res = await fetch(validationUrl)
-  if (!res.ok) {
-    return {}
-  }
   if (res.status === 200) {
     const body = await res.text()
-    return {}
+    console.log(body)
+    return { message: body }
   }
   return {}
 }
