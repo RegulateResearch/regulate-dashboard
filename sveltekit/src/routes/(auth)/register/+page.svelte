c1f414945f3bdb4d97feb48b5eb0f7a3afba7d64
RegulateResearch <michael.jf.jm@gmail.com>
2026-04-04 19:05:47 +0900

    chores: restructure sso button

--- sveltekit/src/routes/(auth)/register/+page.svelte
+++ sveltekit/src/routes/(auth)/register/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import * as Form from '$lib/components/ui/form';
-	import * as Card from '$lib/components/ui/card/index';
-	import { Button } from '$lib/components/ui/button/index';
 	import { Input } from '$lib/components/ui/input/index';
 	import { formSchema, type FormSchema } from './schema';
 	import { zod4Client } from 'sveltekit-superforms/adapters';
@@ -22,62 +20,62 @@
 	const { form: formData, enhance } = form;
 </script>
 
-<AuthForm formTitle="Daftar">
-	{#snippet formBody()}
-		<form method="POST" use:enhance class="flex flex-col gap-6">
-			<Form.Field {form} name="email">
-				<Form.Control>
-					{#snippet children({ props })}
-						<Form.Label>Email</Form.Label>
-						<Input
-							{...props}
-							id="email"
-							type="email"
-							placeholder="email@mail.com"
-							required
-							bind:value={$formData.email}
-						/>
-					{/snippet}
-				</Form.Control>
-				<Form.FieldErrors />
-			</Form.Field>
-			<Form.Field {form} name="password">
-				<Form.Control>
-					{#snippet children({ props })}
-						<Form.Label>Kata Sandi</Form.Label>
-						<Input
-							{...props}
-							id="password"
-							type="password"
-							placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
-							required
-							bind:value={$formData.password}
-						/>
-					{/snippet}
-				</Form.Control>
-				<Form.FieldErrors />
-			</Form.Field>
-			<Form.Field {form} name="passwordConfirmation">
-				<Form.Control>
-					{#snippet children({ props })}
-						<Form.Label>Ulangi Kata Sandi</Form.Label>
-						<Input
-							{...props}
-							id="password"
-							type="password"
-							placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
-							required
-							bind:value={$formData.passwordConfirmation}
-						/>
-					{/snippet}
-				</Form.Control>
-				<Form.FieldErrors />
-			</Form.Field>
-			<Form.Button type="submit" class="w-full">Daftar</Form.Button>
-		</form>
-	{/snippet}
+<AuthForm formTitle="Daftar" showLogo showSSOButton {formBody} {switchFormBtn} />
 
-	{#snippet switchFormBtn()}
-		<span>Sudah punya akun? <a href="/login" class="text-yellow-500">Masuk</a></span>
-	{/snippet}
-</AuthForm>
+{#snippet formBody()}
+	<form method="POST" use:enhance class="flex flex-col gap-6">
+		<Form.Field {form} name="email">
+			<Form.Control>
+				{#snippet children({ props })}
+					<Form.Label>Email</Form.Label>
+					<Input
+						{...props}
+						id="email"
+						type="email"
+						placeholder="email@mail.com"
+						required
+						bind:value={$formData.email}
+					/>
+				{/snippet}
+			</Form.Control>
+			<Form.FieldErrors />
+		</Form.Field>
+		<Form.Field {form} name="password">
+			<Form.Control>
+				{#snippet children({ props })}
+					<Form.Label>Kata Sandi</Form.Label>
+					<Input
+						{...props}
+						id="password"
+						type="password"
+						placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
+						required
+						bind:value={$formData.password}
+					/>
+				{/snippet}
+			</Form.Control>
+			<Form.FieldErrors />
+		</Form.Field>
+		<Form.Field {form} name="passwordConfirmation">
+			<Form.Control>
+				{#snippet children({ props })}
+					<Form.Label>Ulangi Kata Sandi</Form.Label>
+					<Input
+						{...props}
+						id="password"
+						type="password"
+						placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
+						required
+						bind:value={$formData.passwordConfirmation}
+					/>
+				{/snippet}
+			</Form.Control>
+			<Form.FieldErrors />
+		</Form.Field>
+		<Form.Button type="submit" class="w-full">Daftar</Form.Button>
+	</form>
+{/snippet}
+
+{#snippet switchFormBtn()}
+	<span>Sudah punya akun? <a href="/login" class="text-yellow-500">Masuk</a></span>
+{/snippet}
---
+++ sveltekit/src/routes/(auth)/ssoButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { Button } from '$lib/components/ui/button';
+
+	const handleClick = async () => {
+		window.location.href = `https://sso.ui.ac.id/cas2/login?service=${encodeURIComponent(`${page.url.origin}/sso`)}`;
+	};
+</script>
+
+<Button variant="outline" class="w-full" onclick={handleClick}>
+	<enhanced:img src="$lib/assets/ui-logo.png" alt="Logo UI" height="24" />
+	Masuk dengan SSO UI
+</Button>
