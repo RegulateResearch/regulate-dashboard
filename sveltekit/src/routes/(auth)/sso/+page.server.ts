4bc680c88d7f3dd271f80c73a08a3aa2c3d12277
RegulateResearch <michael.jf.jm@gmail.com>
2026-04-04 19:06:51 +0900

    temp: show sso api message for experimental testing

--- sveltekit/src/routes/(auth)/sso/+page.server.ts
+++ sveltekit/src/routes/(auth)/sso/+page.server.ts
@@ -1,16 +1,22 @@
-import { fail } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
-import { message } from "valibot"
+import { sso } from '$lib/server/api/auth';
+import { fail } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ url, fetch }) => {
+export const load: PageServerLoad = async ({ url }) => {
   const ticket = url.searchParams.get('ticket')
-  const service = encodeURIComponent("http://localhost:5173/")
-  const validationUrl = `https://sso.ui.ac.id/cas2/serviceValidate?ticket=${ticket}&service=${service}`
-  const res = await fetch(validationUrl)
-  if (res.status === 200) {
-    const body = await res.text()
-    console.log(body)
-    return { message: body }
+  const service = encodeURIComponent(`${url.origin}/sso`)
+  try {
+    if (!ticket) throw new Error('No ticket provided')
+    await sso({
+      ticket,
+      service: service
+    })
+  } catch (err) {
+    return {
+      message: 'SSO failed: ' + (err instanceof Error ? err.message : 'Unknown error')
+    }
   }
-  return {}
+  return {
+    message: 'SSO success'
+  }
 }
--- sveltekit/src/routes/(auth)/sso/+page.svelte
+++ sveltekit/src/routes/(auth)/sso/+page.svelte
@@ -1,6 +1,20 @@
-<div class="flex flex-col items-center justify-center gap-4">
-	<enhanced:img src="$lib/assets/ui-logo.png" alt="Logo UI" height="64" />
-	<p class="text-center">
-		Sedang menyambungkan dengan layanan SSO Universitas Indoensia. Mohon menunggu.
-	</p>
-</div>
+<script lang="ts">
+	import AuthForm from '../authForm.svelte';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<AuthForm {formBody} />
+
+{#snippet formBody()}
+	<div class="flex flex-col items-center justify-center gap-4">
+		<enhanced:img src="$lib/assets/ui-logo.png" alt="Logo UI" height="64" />
+		<p class="text-center">
+			Sedang menyambungkan dengan layanan SSO Universitas Indoensia. <br /> Mohon menunggu.
+		</p>
+		<code class="text-sm text-gray-500">
+			{data?.message}
+		</code>
+	</div>
+{/snippet}
