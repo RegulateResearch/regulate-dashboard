427df65294527e735f11edcd2238357d510e98c5
RegulateResearch <michael.jf.jm@gmail.com>
2026-04-15 17:42:44 +0900

    feat: authed route require user info data from session api

--- sveltekit/src/routes/(authed)/+layout.server.ts
+++ sveltekit/src/routes/(authed)/+layout.server.ts
@@ -1,8 +1,17 @@
+import { getSession } from '$lib/server/api/auth';
 import type { LayoutServerLoad } from './$types';
-import { redirect, type Actions } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ url, cookies }) => {
-  const authToken = cookies.get('authToken')
-  if (!authToken) throw redirect(301, `/login?redirectTo=${encodeURIComponent(url.pathname)}`)
-  return { status: 'success' }
+  try {
+    const session = await getSession()
+    if (session && session.message) {
+      return { status: 'success' }
+    }
+  } catch {
+    cookies.delete('authToken', {
+      path: '/'
+    })
+    throw redirect(301, `/login?redirectTo=${encodeURIComponent(url.pathname)}`)
+  }
 }
