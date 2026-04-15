77ffb93c2a4bda3f9bfe2027e5810224c2215cf4
RegulateResearch <michael.jf.jm@gmail.com>
2026-04-15 18:05:17 +0900

    chores: linter

--- sveltekit/src/routes/(auth)/ssoButton.svelte
+++ sveltekit/src/routes/(auth)/ssoButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { Button } from '$lib/components/ui/button';
-	import {env} from '$env/dynamic/public';
+	import { env } from '$env/dynamic/public';
 
 	const handleClick = async () => {
 		window.location.href = `${env.PUBLIC_SSO_UI_URL}/login?service=${encodeURIComponent(`${page.url.origin}/sso`)}`;
