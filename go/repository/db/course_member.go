0cdf16800ec84b7e4ca38a9f30b199f4705c0658
Jahns Christian Albert <vanzrael@gmail.com>
2026-05-12 18:53:58 +0700

    feat: update and delete course member

--- go/repository/db/course_member.go
+++ go/repository/db/course_member.go
@@ -13,6 +13,8 @@ import (
 type CourseMemberRepository interface {
 	FindByCourse(ctx typing.Context, course entity.Course) ([]entity.CourseMember, exception.Exception)
 	AddMultiple(ctx typing.Context, newMembers []entity.CourseMember) ([]entity.CourseMember, exception.Exception)
+	DeleteMultiple(ctx typing.Context, courseId typing.ID, memberIds []typing.ID) (int64, exception.Exception)
+	Update(ctx typing.Context, courseId typing.ID, members []entity.CourseMember) ([]entity.CourseMember, exception.Exception)
 }
 
 type courseMemberRepositoryImpl struct {
@@ -109,17 +111,95 @@ func (r courseMemberRepositoryImpl) AddMultiple(ctx typing.Context, newMembers [
 	return res, nil
 }
 
-// func (r courseMemberRepositoryImpl) DeleteMultiple(ctx typing.Context, ids []typing.ID) (int, exception.Exception) {
-// 	querystr := `
-// 		UPDATE course_members cm
-// 		SET deleted_at = NOW()
-// 		FROM (
-// 			VALUES
-// 			%s
-// 		) AS delete_data(id)
-// 		WHERE
-// 			delete_data.id = cm.id AND
-// 			cm.deleted_at IS NULL
-// 		RETURNING cm.id
-// 	`
-// }
+func (r courseMemberRepositoryImpl) DeleteMultiple(ctx typing.Context, courseId typing.ID, memberIds []typing.ID) (int64, exception.Exception) {
+	querystr := `
+		UPDATE course_members cm
+		SET deleted_at = NOW()
+		FROM (
+			VALUES
+			%s
+		) AS delete_data(id)
+		WHERE
+			cm.course_id = $1 AND
+			delete_data.id = cm.id AND
+			cm.deleted_at IS NULL
+	`
+	idxStart := 2
+	rowstr, args, _ := querying.BulkValues(memberIds, idxStart, func(memberId typing.ID, currentIdx int) querying.DataStrArgs {
+		return querying.DataStrArgs{
+			RowStr: fmt.Sprintf("(CAST($%d AS BIGINT))", currentIdx),
+			Args:   []any{memberId},
+		}
+	})
+
+	argsInit := []any{courseId}
+	args = append(argsInit, args...)
+
+	querystr = fmt.Sprintf(querystr, rowstr)
+	res, err := r.executor.ExecContext(ctx, querystr, args...)
+	if err != nil {
+		return -1, repository_exception.WrapQueryexecException(err, "course_member")
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return -1, repository_exception.WrapQueryexecException(err, "course_member")
+	}
+
+	return rowsAffected, nil
+}
+
+func (r courseMemberRepositoryImpl) Update(ctx typing.Context, courseId typing.ID, members []entity.CourseMember) ([]entity.CourseMember, exception.Exception) {
+	querystr := `
+		UPDATE course_members cm
+		SET
+			course_role = update_data.course_role,
+			updated_at = NOW()
+		FROM (
+			VALUES
+			%s
+		) AS update_data(id, course_role)
+		WHERE
+			cm.course_id = $1 AND
+			update_data.id = cm.id AND
+			cm.deleted_at IS NULL
+		RETURNING
+			cm.id, cm.user_id, cm.course_role
+	`
+
+	idxStart := 2
+	rowstr, args, _ := querying.BulkValues(
+		members, idxStart,
+		func(member entity.CourseMember, currentIdx int) querying.DataStrArgs {
+			return querying.DataStrArgs{
+				RowStr: fmt.Sprintf("(CAST($%d AS BIGINT), CAST($%d AS SMALLINT))", currentIdx, currentIdx+1),
+				Args:   []any{member.ID, member.Role},
+			}
+		},
+	)
+
+	querystr = fmt.Sprintf(querystr, rowstr)
+
+	argsInit := []any{courseId}
+	args = append(argsInit, args...)
+
+	rows, err := r.executor.QueryContext(ctx, querystr, args...)
+	if err != nil {
+		return nil, repository_exception.WrapQueryexecException(err, "course_member")
+	}
+	defer r.executor.CloseRows(rows, "course_member - Update")
+
+	res, err := querying.ScanForRows(
+		rows, entity.NewCourseMember,
+		func(rows queryexec.Rows, elem entity.CourseMember) (entity.CourseMember, exception.Exception) {
+			err := rows.Scan(&elem.ID, &elem.User.ID, &elem.Role)
+			return elem, err
+		},
+	)
+
+	if err != nil {
+		return nil, repository_exception.WrapQueryexecException(err, "course_member")
+	}
+
+	return res, nil
+}
