eac843ca62c47493aaf4bf1d1452b376b07d0622
RegulateResearch <michael.jf.jm@gmail.com>
2026-08-18 12:41:45 +0700

    fix: minor styling change for logo and sidebar

--- sveltekit/src/lib/components/ui/loading/loading.svelte
+++ sveltekit/src/lib/components/ui/loading/loading.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-yellow-50/25 backdrop-blur-md"
+	class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-yellow-50/25 backdrop-blur-md z-50"
 >
 	<RegulateLogo animated loop withTitle class="h-auto w-60" />
 </div>
